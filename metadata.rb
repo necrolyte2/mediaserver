25cc6807603e501c1cc70278bb5e5965d6bedb9b
Tyghe Vallard <tyghe.vallard@target.com>
2016-07-04 11:22:41 -0500

    update version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Tyghe Vallard'
 maintainer_email 'vallardt@gmail.com'
 license 'all_rights'
 description 'Setup home media server'
-version '0.1.0'
+version '0.2.0'
 issues_url 'https://github.com/necrolyte2/mediaserver/issues'
 source_url 'https://github.com/necrolyte2/mediaserver'
 
